7e5a7c951fd76bca0a7c889d62560c6e6861f367
Kenneth Ofosu <kofosu89@gmail.com>
2020-03-03 18:10:05 -0500

    added basic game logic, started the app view

--- app/components/hand.js
+++ app/components/hand.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Card from './card';
 
 /**
  * Hand component
- * 
+ *
  * Renders:
  * score
  * cards
- * 
- * @return {reactElement} markup
+ *
+ * @return {ReactElement} markup
  */
-const Hand = () => {
-  return (
-    <div className="hand">
-      <p>
-        <strong>Score:</strong>
-      </p>
-      <div className="cards">
-        <Card />
-      </div>
-    </div>
-  );
+
+/**
+ * Hand component where dealt cards are rendered.
+ *
+ * @class      Hand (name)
+ * @param      {Object}       props        Component properties
+ * @param      {Array}        props.cards  The cards
+ * @param      {Integer}      props.score  The score
+ * @return     {ReactElement} markup
+ */
+const Hand = ({ cards, score }) => {
+    return (
+        <div className="hand">
+            {score &&
+                <p className="score">
+                    <strong>Score:</strong> {score}
+                </p>
+            }
+            <div className="cards">
+                {cards.map((card, i) =>
+                    <Card
+                        rank={card.rank}
+                        suit={card.suit}
+                        key={i}
+                    />
+                )}
+            </div>
+        </div>
+    );
 }
+
+/**
+ * Defines property types for this component.
+ */
+Hand.propTypes = {
+    cards: PropTypes.array.isRequired,
+    score: PropTypes.number,
+};
 
 export default Hand;
--- app/components/info.js
+++ app/components/info.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 /**
  * Info component
- * 
+ *
  * Renders:
  * round number
- * outcome (win, lost, pct%)
- * 
+ * outcome (win, lost, pust)
+ *
  * @return {ReactElement} markup
  */
-const Info = () => {
-  return (
-    <div className="info">
-        <p>
-            <strong>Round:</strong>
-        </p>
-        <p>
-            <strong>Outcome:</strong>
-        </p>
-    </div>
-  );
+
+/**
+ * Info component where game info is rendered.
+ *
+ * @class      Info (name)
+ * @param      {Object}       props        Component properties
+ * @param      {Integer}      props.round  Game's round
+ * @return     {ReactElement} markup
+ */
+const Info = ({ round }) => {
+    return (
+        <div className="info">
+            <p>
+                <strong>Round:</strong> {round}
+            </p>
+            <p>
+                <strong>Outcome:</strong>
+            </p>
+        </div>
+    );
 }
+
+/**
+ * Defines property types for this component.
+ */
+Info.propTypes = {
+    round: PropTypes.number.isRequired,
+};
 
 export default Info;
